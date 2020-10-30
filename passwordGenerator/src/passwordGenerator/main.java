cb13e048e41a41c03585cbf3332336b0ac85fb30
Benjamin Rafter <brafter@outlook.com>
2020-10-29 21:23:10 -0500

    Added frame

--- passwordGenerator/src/passwordGenerator/main.java
+++ passwordGenerator/src/passwordGenerator/main.java
@@ -1,10 +1,22 @@
 package passwordGenerator;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 public class main {
 
+	public static void startGen() {
+		JFrame f = new JFrame();
+		
+		f.setSize(400, 500);
+		f.setLayout(null); 
+		f.setVisible(true);
+		f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Working");
+		startGen();
 	}
 
 }
